0c27f2bd09043e79a2f59e725e39dd856a9960c3
nz663633 <hyunjisim3@gmail.com>
2025-11-07 12:49:32 +0900

    feat: add room array to store input

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
-        var table = document.createElement("table");
-        for (var i = 0; i < 9; i++) {
-            var tr = document.createElement("tr");
-            table.appendChild(tr);
-            for (var j = 0; j < 9; j++) {
-                var td = document.createElement("td");
-                var input = document.createElement("input");
-                tr.appendChild(td);
-                td.appendChild(input);
-            }
-        }
-        document.querySelector(".chart").appendChild(table);
+var table = document.createElement("table");
+const room = [];
+ for (var i = 0; i < 9; i++) {
+    var tr = document.createElement("tr");
+    table.appendChild(tr);
+    for (var j = 0; j < 9; j++) {
+        var td = document.createElement("td");
+        var input = document.createElement("input");
+        tr.appendChild(td);
+        td.appendChild(input);
+        room.push(input);
+    }
+}
+document.querySelector(".chart").appendChild(table);
+
+
+const random = Math.floor(Math.random() * 9 + 1);
+input.value = random;
