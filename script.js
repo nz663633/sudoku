77d561a1962691dcb318b48f51c5242f97ecec5f
nz663633 <hyunjisim3@gmail.com>
2025-11-10 20:38:57 +0900

    refactor: replace var with let

--- script.js
+++ script.js
@@ -20,12 +20,12 @@ function shuffle(room) {
 
 var table = document.createElement("table");
 
- for (var i = 0; i < 9; i++) {
-    var tr = document.createElement("tr");
+ for (let i = 0; i < 9; i++) {
+    let tr = document.createElement("tr");
     table.appendChild(tr);
-    for (var j = 0; j < 9; j++) {
-        var td = document.createElement("td");
-        var input = document.createElement("input");
+    for (let j = 0; j < 9; j++) {
+        let td = document.createElement("td");
+        let input = document.createElement("input");
         input.type = "text";
         tr.appendChild(td);
         td.appendChild(input);
